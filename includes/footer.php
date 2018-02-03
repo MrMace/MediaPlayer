857933eedaff8e865573f28c2c832f8a7e82605e
mattt <mace.matt@umail.iu.edu>
2018-02-03 09:37:09 -0500

    Organization (put header in footer in own file)

---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 2/3/2018
+ * Time: 9:34 AM
+ */
+
+?>
+
+
+</div>
+</div>
+</div>
+
+<?php include("includes/playerControlBar.php"); ?>
+
+
+</div>
+
+
+</body>
+</html>
+
+
+
+
---
+++ includes/header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 2/3/2018
+ * Time: 9:32 AM
+ */
+
+?>
+
+
+<?php
+
+include("includes/config.php");
+
+//log user out on refresh
+//session_destroy();
+
+//checks to see if user is signed in if so move to index if not back to sign in page.
+if (isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+    header("LOCATION: register.php");
+}
+
+
+?>
+<html>
+<head>
+    <title>Mace Media Player</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+<div id="mainContainer">
+
+    <div id="containerTop">
+        <?php include("includes/navBarContainer.php"); ?>
+
+
+        <div id="mainScreenContainer">
+
+            <div id="mainContent">
+
+
