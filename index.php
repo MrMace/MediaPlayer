7f700bff455d32ca5fa62400e7f2a63a20f3e29b
mattt <mace.matt@umail.iu.edu>
2018-02-03 09:58:31 -0500

    Mysql Query album Info

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
 <?php include("includes/header.php"); ?>
 
 
+<h1 class="pageHeaderH1">More Music You Might Enjoy</h1>
+
+<div class="gridContainer">
+
+    <?php $queryAlbum = mysqli_query($connection, "SELECT * FROM albums");
+
+    while ($row = mysqli_fetch_array($queryAlbum)) {
+        echo $row['title'] . "<br>";
+
+    }
+    ?>
+</div>
 
 <?php include("includes/footer.php"); ?>
 
