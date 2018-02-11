413d308c8630c64165593a88d89a11038499385e
mattt <mace.matt@umail.iu.edu>
2018-02-11 00:51:52 -0500

    Created New class for tracks

--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -5,3 +5,82 @@
  * Date: 2/10/2018
  * Time: 7:58 PM
  */
+
+?>
+
+<?php
+
+class Song
+{
+    private $connection;
+    private $id;
+    private $mysqliData;
+    private $albumId;
+    private $artistId;
+    private $genre;
+    private $title;
+    private $path;
+    private $duration;
+
+    public function __construct($connection, $id)
+    {
+        $this->connection = $connection;
+        $this->id = $id;
+
+
+        $query = mysqli_query($this->connection, "SELECT * FROM songs WHERE id='$this->id'");
+        $this->mysqliData = mysqli_fetch_array($query);
+
+        $this->title = $this->mysqliData['title'];
+        $this->artistId = $this->mysqliData['artist'];
+        $this->albumId = $this->mysqliData['album'];
+        $this->genre = $this->mysqliData['genre'];
+        $this->duration = $this->mysqliData['duration'];
+        $this->path = $this->mysqliData['path'];
+
+    }
+
+    /**
+     * @return mixed
+     */
+
+    public function getMysqliData()
+    {
+        return $this->mysqliData;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getArtist()
+    {
+        return new Artist($this->connection, $this->artistId);
+    }
+
+    public function getAlbum()
+    {
+        return new Album($this->connection, $this->albumId);
+    }
+
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+
+}
+
+
+?>
--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,7 @@
 include("includes/config.php");
 include("includes/classes/Artist.php");
 include("includes/classes/Album.php");
+include("includes/classes/Song.php");
 
 //log user out on refresh
 //session_destroy();
