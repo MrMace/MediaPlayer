e17373b4c902eed4c6557e84ab6148716da70c53
mattt <mace.matt@umail.iu.edu>
2018-02-22 18:26:17 -0500

    Track duration

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 var currentPlayList = [];
 var audioElement;
 
+function formatTime(seconds) {
+    var time = Math.round(seconds);
+    //rounds down
+    var mins = Math.floor(time / 60);
+    var seconds = time - (mins * 60);
 
+    return mins + ":" + seconds;
+}
 function Audio() {
 
 
     this.currentPlaying;
     this.audio = document.createElement('audio');
+
+    this.audio.addEventListener("canplay", function () {
+        //refers to obj event called on.
+        var duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
+    });
 
     this.setTrack = function (track) {
         this.currentlyPlaying = track;
