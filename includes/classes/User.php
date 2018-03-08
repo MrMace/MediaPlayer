0457b3b169774201acadfb2d6ee708c6036461a5
mattt <mace.matt@umail.iu.edu>
2018-03-07 19:54:12 -0500

    profilePage

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -27,6 +27,12 @@ class User
         return $this->username;
     }
 
+    public function getFAndLName(){
+        $query= mysqli_query($this->connection, "SELECT concat(firstName, ' ', lastName) as 'name' FROM users WHERE username='$this->username'");
+        $row = mysqli_fetch_array($query);
+        return $row['name'];
+    }
+
 }
 
 
---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 3/7/2018
+ * Time: 6:12 PM
+ */?>
+
+
+<?php include("includes/includedFiles.php");?>
+
+<div class="albumTopInfo">
+    <div class="centerSection">
+        <div class="artistInfo">
+            <h1 class="artistName"><?php echo $userLoggedIn->getFAndLName(); ?></h1>
+        </div>
+    </div>
+</div>
